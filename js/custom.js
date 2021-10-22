484d98b3ef470f1d7184423c507e937ff6f5317e
webworld420 <76555387+webworld420@users.noreply.github.com>
2021-10-22 11:02:12 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+	'use':'strict'
+
+
+$('.bar').click(function(){
+	$('.menu').toggle(500);
+})
+
+})
+
+alert("wellcome")
+
+
+
+$(".bar i").on("click", function(){
+				$(".menu").toggle(500);
+			})
+		$(".time-out").on("click", function(){
+				$(".menu").toggle(500);
+			})
